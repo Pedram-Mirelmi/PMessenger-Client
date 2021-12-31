b4eccd1514ee8d6e987600f8b3a55f5dd641031e
Pedram <mirelmipedram@gmail.com>
2021-12-31 22:38:54 +0330

    changed server's database to Postgres

--- Server/DatabaseQueries.sql
+++ Server/DatabaseQueries.sql
@@ -1,36 +1,47 @@
-DROP DATABASE IF EXISTS MessengerDB;
-CREATE DATABASE MessengerDB;
-USE MessengerDB;
+DROP VIEW IF EXISTS text_messages_view;
+DROP VIEW IF EXISTS private_chats_view;
 
-CREATE TABLE users
+DROP TABLE IF EXISTS text_messages;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS private_chats;
+DROP TABLE IF EXISTS chat_attends;
+DROP TABLE IF EXISTS chat_envs;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS users;
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users 
 (
-    user_id             INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id             BIGSERIAL NOT NULL PRIMARY KEY,
     username            VARCHAR(200) UNIQUE NOT NULL,
     password            VARCHAR(200) NOT NULL ,
     name                VARCHAR(200),
-    created_at          DATE DEFAULT(NOW())
+    created_at          DATE NOT NULL DEFAULT(NOW())
 );
 
+DROP TABLE IF EXISTS contacts;
 CREATE TABLE contacts
 (
-    user_id             INT UNSIGNED NOT NULL ,
-    contact_id          INT UNSIGNED NOT NULL ,
+    user_id             BIGINT NOT NULL ,
+    contact_id          BIGSERIAL NOT NULL ,
     PRIMARY KEY (user_id, contact_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ,
     FOREIGN KEY (contact_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS chat_envs;
 CREATE TABLE chat_envs
 (
-    env_id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    created_at      DATETIME NOT NULL DEFAULT NOW()
+    env_id          BIGSERIAL NOT NULL PRIMARY KEY ,
+    created_at      DATE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE private_chats
 (
-    env_id          INT UNSIGNED PRIMARY KEY ,
-    first_person    INT UNSIGNED NOT NULL ,
-    second_person   INT UNSIGNED NOT NULL ,
+    env_id          BIGINT PRIMARY KEY ,
+    first_person    BIGINT NOT NULL ,
+    second_person   BIGINT NOT NULL ,
     UNIQUE (first_person, second_person),
     FOREIGN KEY (env_id) REFERENCES chat_envs(env_id),
     FOREIGN KEY (first_person) REFERENCES users(user_id),
@@ -40,8 +51,8 @@ CREATE TABLE private_chats
 
 CREATE TABLE chat_attends
 (
-    user_id         INT UNSIGNED NOT NULL ,
-    env_id          INT UNSIGNED NOT NULL ,
+    user_id         BIGINT NOT NULL ,
+    env_id          BIGINT NOT NULL ,
     PRIMARY KEY (user_id, env_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE NO ACTION ,
     FOREIGN KEY (env_id) REFERENCES chat_envs(env_id) ON DELETE CASCADE
@@ -49,17 +60,17 @@ CREATE TABLE chat_attends
 
 CREATE TABLE messages
 (
-    message_id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    env_id          INT UNSIGNED NOT NULL ,
-    owner_id        INT UNSIGNED NOT NULL ,
-    created_at      TIMESTAMP DEFAULT NOW(),
+    message_id      BIGSERIAL NOT NULL PRIMARY KEY ,
+    env_id          BIGINT NOT NULL ,
+    owner_id        BIGINT NOT NULL ,
+    created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY(owner_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (env_id) REFERENCES chat_envs(env_id) ON DELETE CASCADE
 );
 
 CREATE TABLE text_messages
 (
-    messages_id     INT UNSIGNED PRIMARY KEY ,
+    messages_id     BIGINT NOT NULL PRIMARY KEY ,
     message_text    TEXT NOT NULL ,
     FOREIGN KEY (messages_id) REFERENCES messages(message_id)
 );
@@ -85,4 +96,5 @@ CREATE VIEW private_chats_view AS
         ON ce.env_id = pc.env_id;
 
 
+select * from users;
 
--- Server/ServerPackages/DatabaseConnector.hpp
+++ Server/ServerPackages/DatabaseConnector.hpp
@@ -1,98 +1,100 @@
 #pragma once
 #include <list>
-#include <cppconn/connection.h>
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/statement.h>
-#include <cppconn/resultset.h>
-#include <cppconn/resultset_metadata.h>
+#include <pqxx/pqxx>
+
 #include <jsoncpp/json/json.h>
 #include <mutex>
+#include "../../Commons/stringTools.hpp"
+#include "../../Commons/KeyWords.hpp"
 
-typedef Json::Value QryRes;
+
 typedef Json::Value msg_t;
 typedef unsigned long int id_T;
+typedef Json::Value JsonObj;
+typedef Json::Value JsonArr;
+
 // typedef std::unordered_set<std::string> set;
 
-static std::mutex db_lock;
-static sql::Driver* ThsGetDriverInstance()
-{ 
-    std::lock_guard<std::mutex> lock(db_lock);
-    return get_driver_instance();
-} 
 
 class DBConnector
 {
 private:
-    sql::Driver* driver;
-    sql::Connection* connection;
-    sql::Statement* statement;
+    pqxx::connection m_db_connection;
 public:
 
     DBConnector()
+        :m_db_connection("dbname = MessengerDB user = postgres password = root "
+                         "hostaddr = 127.0.0.1 port = 5432")
     {
-        this->driver = ThsGetDriverInstance();
-        connection = driver->connect("localhost", "root", "root");
-        connection->setSchema("Telegram");
-        statement = connection->createStatement();
+        if(this->m_db_connection.is_open())
+            std::cout << "Successfuly connected to DB" << std::endl;
+        else
+        {
+            std::cerr << "DB didn't open!!!" << std::endl;
+            throw std::runtime_error("DB didn't open!!!");
+        }
     }
     DBConnector(const DBConnector&) = delete;
 
-    std::string executeQry(const std::string& query, bool& success)
+    bool execTransactionQuery(const std::string& query)
     {
         try
         {
-            statement->execute(query);
-            success = true;
-            return std::string("Successfully executed") + query;
+            pqxx::work work(this->m_db_connection);
+            work.exec(query);
+            work.commit();
+            return true;
         }
         catch(const std::exception& e)
         {
-            std::cerr << "Couldn't execute query <" << query << "> ====> " << e.what() << '\n';
-            success = false;
-            return e.what();
+            std::cerr << e.what() << std::endl;
+            return false;
         }   
     }
 
-    void singleSELECT(const std::string& query, QryRes& result)
+    bool singleSELECT(const std::string& query, JsonObj& result)
     {
-        Json::Value columns;
+        std::vector<const char*> columns;
         try
         {
-            auto result_set = statement->executeQuery(query);
-            setCurrentColumns(columns, result_set);
-            if(result_set->next())
-                for (uint8_t i = 1; i <= columns.size(); i++)
-                    result[columns[i - 1].asString()] = result_set->getString(i).c_str();
-            delete result_set;
+            pqxx::nontransaction nontrans(this->m_db_connection);
+            pqxx::result db_result(nontrans.exec(query));
+            const pqxx::row& the_row = db_result[0];
+            
+            this->setCurrentColumns(db_result, columns);
+            for (unsigned int i = 0; i < db_result.columns(); i++)
+                result[columns[i]] = the_row[i].is_null() ? "" : the_row[i].as<std::string>();
+            return true;
         }
-        catch (const std::exception& e)
+        catch(const std::exception& e)
         {
-            std::cerr << e.what() << '\n';
+            std::cerr << e.what() << std::endl;
+            return false;
         }
+        
     };
  
-    std::string SELECT(const std::string& query, QryRes& result)
+    bool SELECT(const std::string& query, JsonArr& result)
     {
-        Json::Value columns;
+        std::vector<const char*> columns;
         try
         {
-            auto result_set = statement->executeQuery(query);
-            setCurrentColumns(columns, result_set);
-            result.append(columns);
-            while (result_set->next())
+            pqxx::nontransaction nontrans(this->m_db_connection);
+            pqxx::result db_result(nontrans.exec(query));
+            this->setCurrentColumns(db_result, columns);
+            for (const auto& row : db_result)
             {
-                Json::Value new_row;
-                for (uint32_t i = 1; i <= columns.size(); i++)
-                    new_row.append(result_set->getString(i).c_str());
-                result.append(new_row);
+                JsonObj new_record;
+                for (unsigned int i = 0; i < db_result.columns(); i++)
+                    new_record[columns[i]] = row[i].is_null() ? "" : row[i].as<std::string>();;
+                result.append(new_record);
             }
-            delete result_set;
-            return fmt::format("succesfullt executed {}", query); 
+            return true;
         }
         catch (const std::exception& e)
         {
-           return e.what();
+           std::cerr << e.what() << std::endl;
+           return false;
         }
     };
 
@@ -100,7 +102,7 @@ public:
     {
         using namespace KeyWords;
         auto create_env_Qry = "INSERT INTO chat_envs() VALUES ();";
-        auto created_env_id = this->getLastInsertId();
+        auto created_env_id = this->getLastInsertId("chat_envs", "env_id");
         auto create_private_chat_query = fmt::format("INSERT INTO private_chats(env_id, first_person, second_person) "
                                                      "VALUES ({},            {},         {});",
                                                              created_env_id, first,      second);
@@ -113,61 +115,57 @@ public:
 
         bool successful1, successful2, successful3;
         
-        this->executeQry(create_env_Qry, successful1);
-        this->executeQry(add_creator, successful2);
-        this->executeQry(add_other, successful3);
+        this->execTransactionQuery(create_env_Qry);
+        this->execTransactionQuery(add_creator);
+        this->execTransactionQuery(add_other);
         
         return successful1 && successful2 && successful3 ? created_env_id : 0;
-    }
-
+    }   
+    
     uint64_t insertNewTextMessage(uint64_t owner_id, uint64_t env_id, const char* message_text)
     {
         using namespace KeyWords;
         auto create_message_query = fmt::format("INSERT INTO messages(env_id, owner_id) "
                                                 "VALUES({},     {});",
-                                                                                               env_id, owner_id);
-        bool succusfull1, succesfull2;
-        this->executeQry(create_message_query, succusfull1);
-        if (succusfull1)
+                                                        env_id, owner_id);
+        if (this->execTransactionQuery(create_message_query))
         {
-            auto created_message_id = this->getLastInsertId();
+            auto created_message_id = this->getLastInsertId("messages", "message_id");
             auto create_text_message_query = fmt::format("INSERT INTO text_messages(message_id, message_text) "
                                                         "VALUES({},                 {}",
                                                                 created_message_id, toRaw(message_text));
-            this->executeQry(create_message_query, succesfull2);
-            return succesfull2 ? created_message_id : 0;
+
+            return  this->execTransactionQuery(create_message_query) ? created_message_id : 0;
         }
         return 0;
-    }
+    }   
 
     uint64_t insertNewUser(const char* username, const char* password)
     {
         using namespace KeyWords;
         auto query = fmt::format("INSERT INTO users(username, password)"
-                                 "VALUES ('{}',      '{}');",
-                                           username, password);
-       
-        bool successful;
-        this->executeQry(query, successful);
-        return successful ? this->getLastInsertId() : 0;
+                                 "VALUES ('{}',     '{}');", username, password);                                 
+        return this->execTransactionQuery(query) ? this->getLastInsertId("users", "user_id") : 0;
     }
 
-protected:
-
-    uint64_t getLastInsertId()
+protected:  
+public:
+    uint64_t getLastInsertId(const char* tablename, const char* id_field)
     {
         JsonObj result;
-        this->singleSELECT("SELECT LAST_INSERT_ID() AS id;", result);
-        return result["id"].asUInt64();
+        auto query = fmt::format("SELECT currval(pg_get_serial_sequence('{}', '{}')) AS id;"
+                                                                , tablename, id_field);
+        this->singleSELECT(query, result);
+        return stoi(result["id"].asString());
     }
 
 private:
-    inline void setCurrentColumns(msg_t& columns_list, sql::ResultSet* result_set)
+    inline void setCurrentColumns(const pqxx::result& result_set, std::vector<const char*>& columns)
     {
-        auto meta_data = result_set->getMetaData();
-        auto n = meta_data->getColumnCount();
-        for (unsigned int i = 1; i <= n; i++)
-            columns_list.append(meta_data->getColumnName(i).c_str());
+        columns.clear();
+        columns.reserve(result_set.columns());
+        for (unsigned int i = 0; i < result_set.columns(); i++)
+            columns.emplace_back(result_set.column_name(i));
     }
 
 
